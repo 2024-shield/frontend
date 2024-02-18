4ab79da2ee9df5fd8d4c95daf89522ffcf9e4337
김세희 <lolry0207@sookmyung.ac.kr>
2024-02-18 22:52:03 +0900

    Add Reportwrite Style

--- src/pages/Reportwritepage.tsx
+++ src/pages/Reportwritepage.tsx
@@ -8,7 +8,6 @@ const ReportwritepageStyle = styled.div`
     padding: 20px;
     display: flex;
     flex-direction: column;
-    y-overflow: scroll;
 `
 
 const InputButtonStyle = styled.div`
@@ -21,7 +20,7 @@ const InputButtonStyle = styled.div`
 `
 
 const InfoNameStyle = styled.div`
-
+    display: flex;
 `
 
 const InfoEventStyle = styled.div`
@@ -32,12 +31,17 @@ const InfoDamageStyle = styled.div`
 
 `
 
+const InfoPeopleNum = styled.div`
+    display: flex;
+`
+
 const InfoMobilizationStyle = styled.div`
 
 `
 
 const InfoActionStyle = styled.div`
-
+    width: 342px;
+    align-self: flex-start;
 `
 
 const InputboxStyle = styled.div`
@@ -49,8 +53,15 @@ const InputboxStyle = styled.div`
 `
 
 const H5Style = styled.h5`
-    margin: 5px 0px;
+    margin: 5px 3px;
     font-size: 13px;
+`
+
+const H4Style = styled.h4`
+    width: 300px;
+    margin: 10px 0px 15px 0px;
+    padding-top: 10px;
+    border-top: 2px dotted navy;
 `
 
 const InputStyle = styled.input`
@@ -63,6 +74,16 @@ const InputStyle = styled.input`
     justify-content: center;
 `
 
+const FormDivStyle = styled.div`
+    
+`
+
+const ConnectDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
 const Reportwrite = () => {
     return(
         <div className="Reportpage">
@@ -72,16 +93,22 @@ const Reportwrite = () => {
 
                 <InputboxStyle>
                     <InfoNameStyle>
-                        <H5Style>보고책임자</H5Style>
-                        <InputStyle />
+                        <FormDivStyle>
+                            <H5Style>작성자</H5Style>
+                            <InputStyle id="width_middle"/>
+                        </FormDivStyle>
 
-                        <H5Style>관할 소방서</H5Style>
-                        <InputStyle />
+                        <FormDivStyle>
+                            <H5Style>관할 소방서</H5Style>
+                            <InputStyle id="width_middle"/>
+                        </FormDivStyle>
                     </InfoNameStyle>
 
                     <InfoEventStyle>
+                        <H4Style>발생 개요</H4Style>
                         <H5Style>일시</H5Style>
-                        <InputStyle />
+                        <InputStyle type="date"/>
+                        <InputStyle type="time"/>
 
                         <H5Style>장소</H5Style>
                         <InputStyle />
@@ -91,28 +118,59 @@ const Reportwrite = () => {
                     </InfoEventStyle>
 
                     <InfoDamageStyle>
+                        <H4Style>피해 상황</H4Style>
                         <H5Style>인명 피해</H5Style>
-                        <H5Style>@명 (사망 @, 부상 @)</H5Style>
-                        <InputStyle />
+                        <InfoPeopleNum>
+                            <FormDivStyle>
+                                <H5Style>사망자 수</H5Style>
+                                <ConnectDiv>
+                                    <InputStyle id="width_short"/>
+                                    <H5Style>명</H5Style>
+                                </ConnectDiv>
+                            </FormDivStyle>
+                            <FormDivStyle>
+                                <H5Style>부상자 수</H5Style>
+                                <ConnectDiv>
+                                    <InputStyle id="width_short"/>
+                                    <H5Style>명</H5Style>
+                                </ConnectDiv>
+                            </FormDivStyle>
+                        </InfoPeopleNum>
 
                         <H5Style>사망자 명단</H5Style>
-                        <InputStyle  id="height_long"/>
+                        <InputStyle id="height_long"/>
 
                         <H5Style>부상자 명단</H5Style>
-                        <InputStyle  id="height_long"/>
+                        <InputStyle id="height_long"/>
 
                         <H5Style>재산 피해</H5Style>
                         <InputStyle />
                     </InfoDamageStyle>
 
                     <InfoMobilizationStyle>
-                        <H5Style>동원 상황</H5Style>
-                        <InputStyle id="width_short"/>
+                        <H4Style>동원 상황</H4Style>
+                        <InfoPeopleNum>
+                            <FormDivStyle>
+                                <H5Style>인원</H5Style>
+                                <ConnectDiv>
+                                    <InputStyle id="width_short"/>
+                                    <H5Style>명</H5Style>
+                                </ConnectDiv>
+                            </FormDivStyle>
+                            
+                            <FormDivStyle>
+                                <H5Style>장비</H5Style>
+                                <ConnectDiv>
+                                    <InputStyle id="width_short"/>
+                                    <H5Style>대</H5Style>
+                                </ConnectDiv>
+                            </FormDivStyle>
+                        </InfoPeopleNum>
                     </InfoMobilizationStyle>
 
                     <InfoActionStyle>
-                        <H5Style>조치 사항</H5Style>
-                        <InputStyle  id="height_long"/>
+                        <H4Style>조치 사항</H4Style>
+                        <InputStyle id="height_long"/>
                     </InfoActionStyle>
                 </InputboxStyle>
 
